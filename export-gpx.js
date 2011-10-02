61f411f4b5cba3a240b141050644aedbb5100c2d
Robert Kosara <rkosara@me.com>
2011-10-01 21:59:15 -0400

    Export heart rate data when creating GPX files.

--- export-gpx.js
+++ export-gpx.js
@@ -29,52 +29,77 @@ exports.exportGPX = function(dbClient, runID, response) {
 	gpxNode.att('version', '1.1');
 	gpxNode.att('creator', 'http://eagerfeet.org/');
 	gpxNode.att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+	gpxNode.att('xmlns:gpxtpx', 'http://www.garmin.com/xmlschemas/TrackPointExtension/v1');
 	gpxNode.att('xmlns', 'http://www.topografix.com/GPX/1/1');
-	gpxNode.att('xsi:schemaLocation', 'http://www.topografix.com/GPX/1/1 http://www.topografix.com/gpx/1/1/gpx.xsd');
+	gpxNode.att('xsi:schemaLocation', 'http://www.topografix.com/GPX/1/1 http://www.topografix.com/gpx/1/1/gpx.xsd\n'+
+		'http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd');
 	
 	dbClient.query('select * from Runs where runID = ?', [runID], function(err, results, fields) {
 	
 		if (results.length > 0) {
 	
 			var run = results[0];
-			
-			var runName = 'Run '+run.runID+', '+run.startTime;
-			
-			var metadata = gpxNode.ele('metadata');
-			metadata.ele('name').txt(runName);
-				
-			var b = metadata.ele('bounds');
-			b.att('minlat', run.minlat);
-			b.att('maxlat', run.maxlat);
-			b.att('minlon', run.minlon);
-			b.att('maxlon', run.maxlon);
-		
-			var trk = gpxNode.ele('trk');
-			
-			trk.ele('name').txt(runName);
-			trk.ele('time').txt(run.startTime);
-			trk.ele('type').txt('Run');
-			
-			var trkSeg = trk.ele('trkseg');
-			
-			dbClient.query('select lat, lon, ele, time from Waypoints where runID = ?', [runID], function(err, results, fields) {
-				
-				results.forEach(function(wp) {
-					var trkPt = trkSeg.ele('trkpt');
-					trkPt.att('lat', wp.lat);
-					trkPt.att('lon', wp.lon);
+	
+			dbClient.query('select offset, heartrate from Heartrates where runID = ? order by offset', [runID], function(err, results, fields) {
+	
+				var heartrates = null;
+	
+				if (results.length > 0) {
+					heartrates = results;
+				}
 					
-					trkPt.ele('ele').txt(wp.ele);
+				var runName = 'Run '+run.runID+', '+run.startTime;
+				
+				var metadata = gpxNode.ele('metadata');
+				metadata.ele('name').txt(runName);
 					
-					trkPt.ele('time').txt(ISODateString(wp.time));
+				var b = metadata.ele('bounds');
+				b.att('minlat', run.minlat);
+				b.att('maxlat', run.maxlat);
+				b.att('minlon', run.minlon);
+				b.att('maxlon', run.maxlon);
+			
+				var trk = gpxNode.ele('trk');
+				
+				trk.ele('name').txt(runName);
+				trk.ele('time').txt(run.startTime);
+				trk.ele('type').txt('Run');
+				
+				var trkSeg = trk.ele('trkseg');
+				
+				dbClient.query('select lat, lon, ele, time from Waypoints where runID = ?', [runID], function(err, results, fields) {
+					
+					var startTime = results[0].time;
+					
+					results.forEach(function(wp) {
+						var trkPt = trkSeg.ele('trkpt');
+						trkPt.att('lat', wp.lat);
+						trkPt.att('lon', wp.lon);
+						
+						trkPt.ele('ele').txt(wp.ele);
+						
+						trkPt.ele('time').txt(ISODateString(wp.time));
+						
+						if (heartrates) {
+							var offset = (wp.time-startTime)/1000;
+							var i = 0;
+							while ((i < heartrates.length-1) && (heartrates[i+1].offset <= offset)) {
+								i += 1;
+							}
+							
+							var ext = trkPt.ele('extensions');
+							var trkPtExt = ext.ele('gpxtpx:TrackPointExtension');
+							trkPtExt.ele('gpxtpx:hr').txt(''+heartrates[i].heartrate);
+						}
+					});
+					
+		//			console.log(gpxDoc.toString());
+					response.setHeader('Content-Disposition', 'attachment; filename=Run-'+fileNameDateString(run.startTime)+'.gpx');
+					response.setHeader('Content-Type', 'application/gpx+xml');
+					response.send(gpxDoc.toString());
+					
+					dbClient.end();
 				});
-				
-	//			console.log(gpxDoc.toString());
-				response.setHeader('Content-Disposition', 'attachment; filename=Run-'+fileNameDateString(run.startTime)+'.gpx');
-				response.setHeader('Content-Type', 'application/gpx+xml');
-				response.send(gpxDoc.toString());
-				
-				dbClient.end();
 			});
 		} else {
 			console.log('run '+runID+' not found in GPX export');
